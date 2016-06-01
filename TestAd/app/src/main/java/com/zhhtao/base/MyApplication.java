3515b206e9c540ec2ab19882041e8234939d54c7
zhhtao89 <375531039@qq.com>
2016-06-01 19:03:56 +0800

    同步

--- TestAd/app/src/main/java/com/zhhtao/base/MyApplication.java
+++ TestAd/app/src/main/java/com/zhhtao/base/MyApplication.java
@@ -4,12 +4,19 @@ import android.app.Application;
 import android.content.Context;
 
 import com.avos.avoscloud.AVOSCloud;
+import com.avos.avoscloud.im.v2.AVIMClient;
+import com.avos.avoscloud.im.v2.AVIMConversation;
+import com.avos.avoscloud.im.v2.AVIMMessage;
+import com.avos.avoscloud.im.v2.AVIMMessageHandler;
+import com.avos.avoscloud.im.v2.AVIMMessageManager;
+import com.avos.avoscloud.im.v2.messages.AVIMTextMessage;
 import com.avoscloud.leanchatlib.controller.ChatManager;
 import com.avoscloud.leanchatlib.utils.ThirdPartUserUtils;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
 import com.zhhtao.leancloud.CustomUserProvider;
+import com.zhhtao.utils.LogUtil;
 
 /**
  * Created by zhangHaiTao on 2016/5/26.
@@ -19,6 +26,8 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         initLeanCloud();
+
+        AVIMMessageManager.registerDefaultMessageHandler(new CustomMessageHandler());
     }
 
     /**
@@ -27,11 +36,11 @@ public class MyApplication extends Application {
     private void initLeanCloud() {
         // 初始化参数依次为 this, AppId, AppKey
         AVOSCloud.initialize(this, "I4AiOzuTXH58hlwzmavFRFuY-gzGzoHsz", "VNNdwm14AProYqwFyd9DkRb6");
-        ChatManager.setDebugEnabled(true);// tag leanchatlib
-        AVOSCloud.setDebugLogEnabled(true);  // set false when release
-        initImageLoader(this);
-        ChatManager.getInstance().init(this);
-        ThirdPartUserUtils.setThirdPartUserProvider(new CustomUserProvider());
+//        ChatManager.setDebugEnabled(true);// tag leanchatlib
+//        AVOSCloud.setDebugLogEnabled(true);  // set false when release
+//        initImageLoader(this);
+//        ChatManager.getInstance().init(this);
+//        ThirdPartUserUtils.setThirdPartUserProvider(new CustomUserProvider());
     }
 
     public static void initImageLoader(Context context) {
@@ -44,4 +53,20 @@ public class MyApplication extends Application {
                 .build();
         ImageLoader.getInstance().init(config);
     }
+
+    public static class CustomMessageHandler extends AVIMMessageHandler {
+        //接收到消息后的处理逻辑
+        @Override
+        public void onMessage(AVIMMessage message,AVIMConversation conversation,AVIMClient client){
+            if(message instanceof AVIMTextMessage){
+                AVIMTextMessage textMessage = (AVIMTextMessage) message;
+                LogUtil.w(textMessage.getText());
+            }
+        }
+
+        public void onMessageReceipt(AVIMMessage message,AVIMConversation conversation,AVIMClient client){
+
+        }
+    }
+
 }
--- TestAd/app/src/main/java/com/zhhtao/leancloud/LeanCloudInitActivity.java
+++ TestAd/app/src/main/java/com/zhhtao/leancloud/LeanCloudInitActivity.java
@@ -6,21 +6,34 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.avos.avoscloud.AVException;
+import com.avos.avoscloud.AVMessage;
 import com.avos.avoscloud.AVObject;
 import com.avos.avoscloud.SaveCallback;
 import com.avos.avoscloud.im.v2.AVIMClient;
+import com.avos.avoscloud.im.v2.AVIMConversation;
 import com.avos.avoscloud.im.v2.AVIMException;
+import com.avos.avoscloud.im.v2.AVIMMessage;
+import com.avos.avoscloud.im.v2.AVIMMessageHandler;
+import com.avos.avoscloud.im.v2.AVIMMessageManager;
 import com.avos.avoscloud.im.v2.callback.AVIMClientCallback;
+import com.avos.avoscloud.im.v2.callback.AVIMConversationCallback;
+import com.avos.avoscloud.im.v2.callback.AVIMConversationCreatedCallback;
+import com.avos.avoscloud.im.v2.messages.AVIMTextMessage;
 import com.avoscloud.leanchatlib.activity.AVChatActivity;
 import com.avoscloud.leanchatlib.controller.ChatManager;
+import com.avoscloud.leanchatlib.model.ConversationType;
 import com.avoscloud.leanchatlib.utils.Constants;
 import com.zhhtao.base.BaseActivty;
+import com.zhhtao.base.MyApplication;
 import com.zhhtao.testad.R;
 import com.zhhtao.utils.LogUtil;
 import com.zhhtao.utils.UIUtils;
 import com.zhhtao.utils.ZhtUtils;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -38,6 +51,10 @@ public class LeanCloudInitActivity extends BaseActivty {
     EditText etTo;
     @Bind(R.id.btn_send_message)
     Button btnSendMessage;
+    @Bind(R.id.et_send_msg)
+    EditText etSendMsg;
+    @Bind(R.id.btn_login)
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -90,6 +107,69 @@ public class LeanCloudInitActivity extends BaseActivty {
 
     @OnClick(R.id.btn_send_message)
     public void btnSendMessage() {
-        
+        from = etFrom.getText().toString().trim();
+        to = etTo.getText().toString().trim();
+        AVIMClient avimClient = AVIMClient.getInstance(from);
+        avimClient.open(new AVIMClientCallback() {
+            @Override
+            public void done(AVIMClient avimClient, AVIMException e) {
+                if (e == null) {
+                    LogUtil.i("login:" + from);
+
+                    Map<String, Object> attrs = new HashMap<>();
+                    attrs.put(ConversationType.TYPE_KEY, ConversationType.Single.getValue());
+                    avimClient.createConversation(Arrays.asList(to), "cvs001", attrs,false, true,  new AVIMConversationCreatedCallback() {
+                        @Override
+                        public void done(AVIMConversation avimConversation, AVIMException e) {
+                            if (e == null) {
+                                LogUtil.w("createConversation");
+                                AVIMTextMessage msg = new AVIMTextMessage();
+                                msg.setText(etSendMsg.getText().toString().trim());
+                                avimConversation.sendMessage(msg, new AVIMConversationCallback() {
+                                    @Override
+                                    public void done(AVIMException e) {
+                                        if (e == null) {
+                                            LogUtil.w("send ok");
+                                        }
+                                    }
+                                });
+                            }
+
+                        }
+                    });
+                }
+
+            }
+        });
+    }
+
+    @OnClick(R.id.btn_login)
+    public void btnLogin() {
+        from = etFrom.getText().toString().trim();
+        AVIMClient avimClient = AVIMClient.getInstance(from);
+        avimClient.open(new AVIMClientCallback() {
+            @Override
+            public void done(AVIMClient avimClient, AVIMException e) {
+                if (e == null) {
+                    LogUtil.i("login:"+from);
+                }
+            }
+        });
+    }
+
+    static class MyCustomMessageHandler extends AVIMMessageHandler {
+        //接收到消息后的处理逻辑
+        @Override
+        public void onMessage(AVIMMessage message,AVIMConversation conversation,AVIMClient client){
+            if(message instanceof AVIMTextMessage){
+                LogUtil.i("here");
+                LogUtil.w(((AVIMTextMessage)message).getText());
+            }
+        }
+
+        public void onMessageReceipt(AVIMMessage message,AVIMConversation conversation,AVIMClient client){
+
+        }
     }
 }
+
